6ce359a44ffb45cdf309259fa8130b91c62f7a25
Wavy <wavy1s@mail.ru>
2018-01-26 22:06:46 +0100

    Added empty some scripts and prefabs

---
+++ Assets/scripts/ZombieMovementBehaviour.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zombieMovementBehaviour : MonoBehaviour {
+
+	public void Move(Command command){
+
+	}
+}
