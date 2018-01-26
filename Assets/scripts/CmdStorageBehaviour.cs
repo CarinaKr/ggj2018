4050623f96f07d5fb052b6d0664a66b99b61ae2e
Wavy <wavy1s@mail.ru>
2018-01-26 22:43:14 +0100

    Added CmdStorageBehaviour

---
+++ Assets/scripts/CmdStorageBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CmdStorageBehaviour : MonoBehaviour {
+
+	private List<Command> commands;
+	public List<Command> Commands {
+		set{commands = value;}
+		get{ return commands;}
+	}
+}
